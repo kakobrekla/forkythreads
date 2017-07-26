b21a315466762edb4e25cb5f7482f79178b99a1d
kakobrekla <maticko@gmail.com>
2017-07-26 20:05:10 +0200

    https://stackoverflow.blog/2017/06/15/developers-use-spaces-make-money-use-tabs/

--- demo-tasks.php
+++ demo-tasks.php
@@ -7,28 +7,27 @@ class DemoTaskA extends Task{
 
 class DemoTaskB extends Task {
 
-	var $foo;
-	
-	function __construct($param){
-		$this->foo = $param;
-	}
+    var $foo;
 
-	function _onInit(){
-		$this->printCLI("Starting.");
-	}
-	
-	function _onExit(){
-		$this->printCLI("Exit.");
-	}
-	
-	function _loop(){
-		$duration = array_rand(array_fill(1, 5, 0));
-		$this->printCLI("foo is $this->foo, im going afk for $duration seconds");
-		$this->_nap($duration);
-	}
-	
-	function printCLI($line){
-		print("[".date("Y-m-d H:i:s")."] (".getmypid().") ". __CLASS__ .": $line \n");
-	}
-	
+    function __construct($param){
+        $this->foo = $param;
+    }
+
+    function _onInit(){
+        $this->printCLI("Starting.");
+    }
+
+    function _onExit(){
+        $this->printCLI("Exit.");
+    }
+
+    function _loop(){
+        $duration = array_rand(array_fill(1, 5, 0));
+        $this->printCLI("foo is $this->foo, im going afk for $duration seconds");
+        $this->_nap($duration);
+    }
+
+    function printCLI($line){
+        print("[".date("Y-m-d H:i:s")."] (".getmypid().") ". __CLASS__ .": $line \n");
+    }
 }
